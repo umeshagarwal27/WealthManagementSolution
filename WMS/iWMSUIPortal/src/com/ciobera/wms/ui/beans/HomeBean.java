fa737314467592a09565ddab46a035b6a8dcd481
umeshagarwal27 <umeshagarwal27@gmail.com>
2017-08-07 12:23:07 +0530

    Initial Project setup - 3.

---
+++ WMS/iWMSUIPortal/src/com/ciobera/wms/ui/beans/HomeBean.java
@@ -0,0 +1,21 @@
+/*****************************************************************************************************
+ ** Program Name            - Home.java
+ ** Program Description     - This class contains the logic to set GlobalInfo upon successful login.
+ ** Date written            - 
+ ** Author                  - Umesh Agarwal
+ ** Additional Information  -
+ ** Copyright notice        -
+ ******************************************************************************************************/
+package com.ciobera.wms.ui.beans;
+
+import oracle.adf.share.logging.ADFLogger;
+
+
+public class HomeBean {
+    public static final ADFLogger LOGGER =
+        ADFLogger.createADFLogger(HomeBean.class);
+
+    public HomeBean() {
+        super();
+    }
+}
