3a7c61f1157a468fa5a2e2b3997e23c1a3aa1998
umeshagarwal27 <umeshagarwal27@gmail.com>
2017-09-08 00:13:58 +0530

    Dashboard UI changes.

--- FWMS/iFWMSOrderManagementUI/src/com/ciobera/fwms/order/ui/beans/OrderManagementBean.java
+++ FWMS/iFWMSOrderManagementUI/src/com/ciobera/fwms/order/ui/beans/OrderManagementBean.java
@@ -8,11 +8,13 @@
  ******************************************************************************************************/
 package com.ciobera.fwms.order.ui.beans;
 
+import com.ciobera.fwms.common.util.bean.GlobalBean;
 import com.ciobera.fwms.common.util.logger.LoggingUtil;
 import com.ciobera.fwms.common.util.utils.common.ADFUtil;
 
 import java.io.Serializable;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,74 +26,74 @@ import javax.el.ValueExpression;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
-import oracle.adf.model.BindingContext;
 import oracle.adf.share.logging.ADFLogger;
 import oracle.adf.view.faces.bi.event.chart.ChartDrillEvent;
 import oracle.adf.view.rich.component.rich.RichPopup;
 
-import oracle.binding.BindingContainer;
 import oracle.binding.OperationBinding;
 
 import org.apache.myfaces.trinidad.event.SelectionEvent;
 
 public class OrderManagementBean implements Serializable {
-    @SuppressWarnings("compatibility:1524626334163952164")
-    private static final long serialVersionUID = -3351246699426832773L;
+    @SuppressWarnings("compatibility:-5064459763820671720")
+    private static final long serialVersionUID = -3351246699426832772L;
     public static final ADFLogger LOGGER = ADFLogger.createADFLogger(OrderManagementBean.class);
     private boolean showGraph = false;
+    private String userId =
+        ((GlobalBean) ADFUtil.evaluateEL("#{GlobalBean}")) != null ?
+        ((GlobalBean) ADFUtil.evaluateEL("#{GlobalBean}")).getUserId() : "SRINI";
+    private Date asOfdate;
     private RichPopup countryPopup;
     private RichPopup exchangePopup;
     private RichPopup assetPopup;
 
+    /**
+     * Default Constructor
+     */
     public OrderManagementBean() {
     }
 
-    public BindingContainer getBindings() {
-        return BindingContext.getCurrent().getCurrentBindingsEntry();
+    public void processAction(ActionEvent ae) {
+        if (!executeMethod("processAsOfDateRecord", true)) {
+            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
+            return;
+        }
+        if (!executeMethod("ExecuteWithParams", true)) {
+            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
+            return;
+        }
+        if (!executeMethod("ExecuteWithParamsCountry", true)) {
+            setShowGraph(Boolean.TRUE);
+            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
+            return;
+        }
+        if (!executeMethod("ExecuteWithParamsCountryD", true)) {
+            setShowGraph(Boolean.TRUE);
+            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
+            return;
+        }
+        if (!executeMethod("ExecuteWithParamsAsset", true)) {
+            setShowGraph(Boolean.TRUE);
+            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
+            return;
+        }
+        if (!executeMethod("ExecuteWithParamsAssetD", true)) {
+            setShowGraph(Boolean.TRUE);
+            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
+            return;
+        }
+        if (!executeMethod("ExecuteWithParamsExchange", true)) {
+            setShowGraph(Boolean.TRUE);
+            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
+            return;
+        }
+        if (!executeMethod("ExecuteWithParamsExchangeD", true)) {
+            setShowGraph(Boolean.TRUE);
+            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
+            return;
+        }
     }
 
-    public void processAction(ActionEvent ae) {
-        if (!executeMethod("processAsOfDateRecord",true)) {
-            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
-            return;
-        } 
-        if (!executeMethod("ExecuteWithParams",true)) {
-            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
-            return;
-        } 
-        if (!executeMethod("ExecuteWithParamsCountry",true)) {
-            setShowGraph(Boolean.TRUE);
-            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
-            return;
-        }
-        if (!executeMethod("ExecuteWithParamsCountryD",true)) {
-            setShowGraph(Boolean.TRUE);
-            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
-            return;
-        }
-        if (!executeMethod("ExecuteWithParamsAsset",true)) {
-            setShowGraph(Boolean.TRUE);
-            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
-            return;
-        }
-        if (!executeMethod("ExecuteWithParamsAssetD",true)) {
-            setShowGraph(Boolean.TRUE);
-            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
-            return;
-        }
-        if (!executeMethod("ExecuteWithParamsExchange",true)) {
-            setShowGraph(Boolean.TRUE);
-            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
-            return;
-        }
-        if (!executeMethod("ExecuteWithParamsExchangeD",true)) {
-            setShowGraph(Boolean.TRUE);
-            //displayErrorPopup(ADFUtil.getUIBundleMsg("UNEXPECTED_ERROR"));
-            return;
-        }
-        
-    }
-    
     /**
      * Helper method to execute Method.
      * @param methodName
@@ -118,8 +120,8 @@ public class OrderManagementBean implements Serializable {
         }
         return resultFlag;
     }
-            
-            
+
+
     /**
      * Helper method to execute Method.
      * @param methodName
@@ -143,6 +145,97 @@ public class OrderManagementBean implements Serializable {
                                          "Exception inside LoginBean.executeMethod() : " + methodName + e.toString());
         }
         return resultMap;
+    }
+
+
+    public void onCountrySelect(ChartDrillEvent chartDrillEvent) {
+        executeMethod("ExecuteWithParamsCountryD", true);
+        if (getCountryPopup() != null) {
+            RichPopup.PopupHints hints = new RichPopup.PopupHints();
+            getCountryPopup().show(hints);
+        }
+    }
+
+    public void onExchangeSelect(ChartDrillEvent chartDrillEvent) {
+        executeMethod("ExecuteWithParamsExchangeD", true);
+        if (getExchangePopup() != null) {
+            RichPopup.PopupHints hints = new RichPopup.PopupHints();
+            getExchangePopup().show(hints);
+        }
+    }
+
+    public void onAssetSelect(ChartDrillEvent chartDrillEvent) {
+        executeMethod("ExecuteWithParamsAssetD", true);
+        if (getAssetPopup() != null) {
+            RichPopup.PopupHints hints = new RichPopup.PopupHints();
+            getAssetPopup().show(hints);
+        }
+    }
+
+    /**
+     * Programmatic invocation of a method that an EL evaluates to.
+     *
+     * @param el EL of the method to invoke
+     * @param paramTypes Array of Class defining the types of the parameters
+     * @param params Array of Object defining the values of the parametrs
+     * @return Object that the method returns
+     */
+    public static Object invokeEL(String el, Class[] paramTypes, Object[] params) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ELContext elContext = facesContext.getELContext();
+        ExpressionFactory expressionFactory = facesContext.getApplication().getExpressionFactory();
+        MethodExpression exp = expressionFactory.createMethodExpression(elContext, el, Object.class, paramTypes);
+        return exp.invoke(elContext, params);
+    }
+
+    /**
+     * Programmatic evaluation of EL.
+     *
+     * @param el EL to evaluate
+     * @return Result of the evaluation
+     */
+    public static Object evaluateEL(String el) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ELContext elContext = facesContext.getELContext();
+        ExpressionFactory expressionFactory = facesContext.getApplication().getExpressionFactory();
+        ValueExpression exp = expressionFactory.createValueExpression(elContext, el, Object.class);
+
+        return exp.getValue(elContext);
+    }
+
+    /**Custom Selection Listener for dvt:pieChart
+     * @param selectionEvent
+     */
+    public void pieChartSelectionListener(SelectionEvent selectionEvent, String elmakeCurrent) {
+        // Makes selected slice as current row
+        //"#{bindings.Employees1.collectionModel.makeCurrent}"
+        invokeEL(elmakeCurrent, new Class[] { SelectionEvent.class }, new Object[] { selectionEvent });
+        // Get the selected row (Use pie chart iterator name)
+
+    }
+
+    public void countrySelectionListener(SelectionEvent selectionEvent) {
+        pieChartSelectionListener(selectionEvent, "#{bindings.FWMSDHoldingAllCountry.collectionModel.makeCurrent}");
+        if (getCountryPopup() != null) {
+            RichPopup.PopupHints hints = new RichPopup.PopupHints();
+            getCountryPopup().show(hints);
+        }
+    }
+
+    public void exchangeSelectionListener(SelectionEvent selectionEvent) {
+        pieChartSelectionListener(selectionEvent, "#{bindings.FWMSDHoldingAllExchange.collectionModel.makeCurrent}");
+        if (getExchangePopup() != null) {
+            RichPopup.PopupHints hints = new RichPopup.PopupHints();
+            getExchangePopup().show(hints);
+        }
+    }
+
+    public void assetSelectionListener(SelectionEvent selectionEvent) {
+        pieChartSelectionListener(selectionEvent, "#{bindings.FWMSDHoldingAllAsset.collectionModel.makeCurrent}");
+        if (getAssetPopup() != null) {
+            RichPopup.PopupHints hints = new RichPopup.PopupHints();
+            getAssetPopup().show(hints);
+        }
     }
 
     public void setShowGraph(boolean showGraph) {
@@ -177,92 +270,19 @@ public class OrderManagementBean implements Serializable {
         return assetPopup;
     }
 
-    public void onCountrySelect(ChartDrillEvent chartDrillEvent) {
-        if (getCountryPopup() != null) {
-            RichPopup.PopupHints hints = new RichPopup.PopupHints();
-            getCountryPopup().show(hints);
-        }
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
-    
-    public void onExchangeSelect(ChartDrillEvent chartDrillEvent) {
-        if (getExchangePopup() != null) {
-            RichPopup.PopupHints hints = new RichPopup.PopupHints();
-            getExchangePopup().show(hints);
-        }
+
+    public String getUserId() {
+        return userId;
     }
-    
-    public void onAssetSelect(ChartDrillEvent chartDrillEvent) {
-        if (getAssetPopup() != null) {
-            RichPopup.PopupHints hints = new RichPopup.PopupHints();
-            getAssetPopup().show(hints);
-        }
+
+    public void setAsOfdate(Date asOfdate) {
+        this.asOfdate = asOfdate;
     }
-    
-    /**
-         * Programmatic invocation of a method that an EL evaluates to.
-         *
-         * @param el EL of the method to invoke
-         * @param paramTypes Array of Class defining the types of the parameters
-         * @param params Array of Object defining the values of the parametrs
-         * @return Object that the method returns
-         */
-        public static Object invokeEL(String el, Class[] paramTypes, Object[] params) {
-            FacesContext facesContext = FacesContext.getCurrentInstance();
-            ELContext elContext = facesContext.getELContext();
-            ExpressionFactory expressionFactory = facesContext.getApplication().getExpressionFactory();
-            MethodExpression exp = expressionFactory.createMethodExpression(elContext, el, Object.class, paramTypes);
 
-            return exp.invoke(elContext, params);
-        }
-
-        /**
-         * Programmatic evaluation of EL.
-         *
-         * @param el EL to evaluate
-         * @return Result of the evaluation
-         */
-        public static Object evaluateEL(String el) {
-            FacesContext facesContext = FacesContext.getCurrentInstance();
-            ELContext elContext = facesContext.getELContext();
-            ExpressionFactory expressionFactory = facesContext.getApplication().getExpressionFactory();
-            ValueExpression exp = expressionFactory.createValueExpression(elContext, el, Object.class);
-
-            return exp.getValue(elContext);
-        }
-
-        /**Custom Selection Listener for dvt:pieChart
-         * @param selectionEvent
-         */
-        public void pieChartSelectionListener(SelectionEvent selectionEvent,String elmakeCurrent) {
-            // Makes selected slice as current row
-            //"#{bindings.Employees1.collectionModel.makeCurrent}"
-            invokeEL(elmakeCurrent, new Class[] { SelectionEvent.class }, new Object[] {
-                     selectionEvent });
-            // Get the selected row (Use pie chart iterator name)
-
-        }
-        
-        public void countrySelectionListener(SelectionEvent selectionEvent){
-            pieChartSelectionListener(selectionEvent,"#{bindings.FWMSDHoldingAllCountry.collectionModel.makeCurrent}");
-            if (getCountryPopup() != null) {
-                RichPopup.PopupHints hints = new RichPopup.PopupHints();
-                getCountryPopup().show(hints);
-            }
-        }
-        
-    public void exchangeSelectionListener(SelectionEvent selectionEvent){
-        pieChartSelectionListener(selectionEvent,"#{bindings.FWMSDHoldingAllExchange.collectionModel.makeCurrent}");
-        if (getExchangePopup() != null) {
-            RichPopup.PopupHints hints = new RichPopup.PopupHints();
-            getExchangePopup().show(hints);
-        }
-    }
-    
-    public void assetSelectionListener(SelectionEvent selectionEvent){
-        pieChartSelectionListener(selectionEvent,"#{bindings.FWMSDHoldingAllAsset.collectionModel.makeCurrent}");
-        if (getAssetPopup() != null) {
-            RichPopup.PopupHints hints = new RichPopup.PopupHints();
-            getAssetPopup().show(hints);
-        }
+    public Date getAsOfdate() {
+        return asOfdate;
     }
 }
